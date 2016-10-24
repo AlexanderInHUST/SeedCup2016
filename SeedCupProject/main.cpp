35c19c2f5199cf6fb06fd585cc3e39ead486f7a5
yifeng tang <yifengtang_hust@outlook.com>
2016-10-24 23:12:33 +0800

    Initial Commit

---
+++ SeedCupProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SeedCupProject
+//
+//  Created by 唐艺峰 on 16/10/24.
+//  Copyright © 2016年 唐艺峰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
